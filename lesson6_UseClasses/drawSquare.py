afa85b659b5d7d51e6e90619958d8afd4fb8dc47
Andrew M. Perez <AxMPerez@gmail.com>
2017-10-08 19:12:43 -0400

    clean code and add 3rd turtle

--- lesson6_UseClasses/drawSquare.py
+++ lesson6_UseClasses/drawSquare.py
@@ -1,6 +1,7 @@
 import turtle
 
-def draw_square():
+
+def draw_shapes():
     window = turtle.Screen()
     window.bgcolor("black")
     
@@ -8,16 +9,28 @@ def draw_square():
     brad.shape("circle")
     brad.color("purple")
     brad.speed(10)
-    
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+
+    i = 0
+    while i < 4:
+        brad.forward(100)
+        brad.right(90)
+        i += 1
+
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
+
+    criss = turtle.Turtle()
+    criss.shape("classic")
+    criss.color("red")
+
+    a = 0
+    while a < 3:
+        criss.backward(100)
+        criss.left(120)
+        a += 1
 
     window.exitonclick()
     
-draw_square()
+draw_shapes()
