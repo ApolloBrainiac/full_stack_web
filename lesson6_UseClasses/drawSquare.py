307c71e8ce93b8b5ed6b45abf9965446a98f4433
Andrew M. Perez <AxMPerez@gmail.com>
2017-10-08 19:25:56 -0400

    make circles from squares

--- lesson6_UseClasses/drawSquare.py
+++ lesson6_UseClasses/drawSquare.py
@@ -1,5 +1,9 @@
 import turtle
 
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
 
 def draw_shapes():
     window = turtle.Screen()
@@ -8,28 +12,24 @@ def draw_shapes():
     brad = turtle.Turtle()
     brad.shape("circle")
     brad.color("purple")
-    brad.speed(10)
+    brad.speed(3)
+    for i in range(1,36):
+        draw_square(brad)
+        brad.right(10)
+    #angie = turtle.Turtle()
+    #angie.shape("arrow")
+    #angie.color("blue")
+    #angie.circle(100)
 
-    i = 0
-    while i < 4:
-        brad.forward(100)
-        brad.right(90)
-        i += 1
+    #criss = turtle.Turtle()
+    #criss.shape("classic")
+    #criss.color("red")
 
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("blue")
-    angie.circle(100)
-
-    criss = turtle.Turtle()
-    criss.shape("classic")
-    criss.color("red")
-
-    a = 0
-    while a < 3:
-        criss.backward(100)
-        criss.left(120)
-        a += 1
+    #a = 0
+    #while a < 3:
+    #    criss.backward(100)
+    #    criss.left(120)
+    #    a += 1
 
     window.exitonclick()
     
