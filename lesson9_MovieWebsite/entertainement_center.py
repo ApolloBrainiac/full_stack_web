cef2f22dad2ce834e846df1b9bb49a04131ec553
Andrew M. Perez <AxMPerez@gmail.com>
2017-10-09 23:05:24 -0400

    add remaining films

--- lesson9_MovieWebsite/entertainement_center.py
+++ lesson9_MovieWebsite/entertainement_center.py
@@ -22,3 +22,15 @@ blade_runner = media.Movie("Blade Runner",
 school_of_rock = media.Movie("School of Rock", "Using rock music to learn",
                              "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille", "Storyline",
+                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris", "Storyline",
+                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=atLg2wQQxvU")
+
+hunger_games = media.Movie("Hunger Games", "Storyline",
+                           "http://uplaod.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                           "https://www.youtube.com/watch?v=PbA63a7H0bo")
